ba20e6e31bc94ef65417c8944ebc304ffaba74a4
tmy-codes <tmyooo0510@gmail.com>
2026-05-16 01:47:23 +0900

    fix render port

--- money_app.py
+++ money_app.py
@@ -930,5 +930,8 @@ def update(id):
     return redirect(f"/?date={selected_date}&month={month}")
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
